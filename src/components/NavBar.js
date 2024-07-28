dae70e66564149e95c4884e1ac79a7a5691be55b
shakuneko <56393184+shakuneko@users.noreply.github.com>
2024-07-28 11:25:24 +0800

    fix_link

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,16 +7,16 @@ export default function NavBar()    {
             
             <div className="header-menu">
                 <div className="header-word">
-                    <NavLink to="/inspirations" className="menu1" activeClassName="menu1--active">
+                    <NavLink to="/" className="menu1" activeClassName="menu1--active">
                         Inspirations
                     </NavLink>
                     <NavLink to="/shop" className="menu1" activeClassName="menu1--active">
                         Shop
                     </NavLink>
-                    <NavLink to="/designers" className="menu1" activeClassName="menu1--active">
+                    <NavLink to="/" className="menu1" activeClassName="menu1--active">
                         Designers
                     </NavLink>
-                    <NavLink to="/about-Us" className="menu1" activeClassName="menu1--active">
+                    <NavLink to="/" className="menu1" activeClassName="menu1--active">
                         About Us
                     </NavLink>
                 </div>
